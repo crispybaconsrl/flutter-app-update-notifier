edc2cd18096591e60c43bde2e627032a530a3c10
Gerald Nuraj <gerald.nuraj@crispybacon.al>
2025-03-07 14:24:40 +0100

    Add usage example for package implementation

---
+++ example/lib/app.dart
@@ -0,0 +1,82 @@
+import 'package:app_update_notifier/app_update_notifier.dart';
+import 'package:example/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final navigatorKey = GlobalKey<NavigatorState>();
+
+  Future<String> fetchMinForcedVesion() async =>
+      Future.delayed(const Duration(seconds: 1), () => '1.0.0');
+
+  Future<String> fetchMinVesion() async =>
+      Future.delayed(const Duration(seconds: 1), () => '1.0.1');
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'App Update Notifier App',
+    navigatorKey: navigatorKey,
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    ),
+    builder: (context, child) {
+      return AppUpdateWidget(
+        navigatorKey: navigatorKey,
+        iosAppStoreId: '284882215',
+        fetchMinVesion: () async => fetchMinVesion(),
+        fetchMinForcedVesion: () async => fetchMinForcedVesion(),
+        onUpdate: (context, storeUrl) async {
+          await showAdaptiveDialog<void>(
+            context: context,
+            builder:
+                (context) => AlertDialog(
+                  title: const Text('Update Available'),
+                  content: const Text(
+                    'A new version of the app is available. Would you like to update?',
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text('Later'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Update'),
+                    ),
+                  ],
+                ),
+          );
+        },
+        onForcedUpdate: (context, storeUrl) async {
+          return await showAdaptiveDialog<bool>(
+            context: context,
+            builder:
+                (context) => AlertDialog(
+                  title: const Text('Update Required'),
+                  content: const Text('Please update the app to continue.'),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: const Text('Close'),
+                    ),
+                  ],
+                ),
+          );
+        },
+        onException: (error, exception) {
+          debugPrint('Error: $error, Exception: $exception');
+        },
+        child: child!,
+      );
+    },
+    home: const HomePage(),
+  );
+}
